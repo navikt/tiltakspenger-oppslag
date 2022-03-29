a835755d424c4dafad55cdf715819443295d7ab3
Sneha Desai <sneha.desai@nav.no>
2022-03-30 00:16:12 +0200

    initial commi

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+
+val javaVersion = JavaVersion.VERSION_11
+
+plugins {
+    kotlin("jvm") version "1.6.10"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = javaVersion.toString()
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = javaVersion.toString()
+        kotlinOptions.freeCompilerArgs += "-opt-in=kotlin.RequiresOptIn"
+    }
+    test {
+        // JUnit 5 support
+        useJUnitPlatform()
+    }
+    shadowJar {
+        dependsOn("MainKt")
+    }
+}
+
+application {
+    mainClass.set("MainKt")
+}
+
+java {
+    sourceCompatibility = javaVersion
+    targetCompatibility = javaVersion
+}
